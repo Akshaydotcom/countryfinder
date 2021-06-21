c2aa428a82d9cb15b3141f04b555a9c2230c84e6
AkshayGB <akshaybhandaryg1@gmail.com>
2021-06-22 03:09:43 +0530

    Added styling for cards, and added filtering functionality

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -24,6 +24,24 @@ export const Homepage = ()=>{
             }
         })
     },[])
+
+    const filterByRegion=(e)=>{
+        if(e.target.text.toLowerCase()!=='all'){
+        axios.get('https://restcountries.eu/rest/v2/region/'+e.target.text.toLowerCase()).then(res=>{
+            if(res.status===200){
+                setAllCountries(res.data)
+            }
+        })}
+        else{
+            axios.get('https://restcountries.eu/rest/v2/all').then(res=>{
+            if(res.status===200){
+                console.log(res)
+                setAllCountries(res.data)
+            }
+        })
+        }
+    }
+
     return (<div className="container" >
         <div className={lightMode?' row header-light':' row header-dark'} >
         <h5 className="col-lg-10 col-md-10 col-sm-5 col-7">Where in the world?</h5>
@@ -38,22 +56,23 @@ export const Homepage = ()=>{
             <Dropdown className="dark-dropdown">
                 <Dropdown.Toggle>Filter by Region</Dropdown.Toggle>
                 <Dropdown.Menu>
-                    <Dropdown.Item>Africa</Dropdown.Item>
-                    <Dropdown.Item>Asia</Dropdown.Item>
-                    <Dropdown.Item>America</Dropdown.Item>
-                    <Dropdown.Item>Europe</Dropdown.Item>
-                    <Dropdown.Item>Oceania</Dropdown.Item>
+                    <Dropdown.Item onClick={filterByRegion}>Africa</Dropdown.Item>
+                    <Dropdown.Item onClick={filterByRegion}>Asia</Dropdown.Item>
+                    <Dropdown.Item onClick={filterByRegion}>Americas</Dropdown.Item>
+                    <Dropdown.Item onClick={filterByRegion}>Europe</Dropdown.Item>
+                    <Dropdown.Item onClick={filterByRegion}>Oceania</Dropdown.Item>
+                    <Dropdown.Item onClick={filterByRegion}>All</Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
         </div>
         <div className="grid">
             {(allCountries && allCountries.length!==0 && allCountries.map((country)=>(
-                <Card className="ml-3 mb-3 mt-2" style={{width:'16rem',display:"inline-flex"}}>
+                <Card className="ml-3 mb-3 mt-2 card-body-dark" style={{width:'16rem',display:"inline-flex"}}>
                     <Card.Img variant="top" src={country.flag} style={{height:'170px'}}></Card.Img>
                     <Card.Body>
                         <Card.Text>
                             <div >
-                                <div>{country.name}</div>
+                                <div className="card-head">{country.name}</div>
                                 <div>Population: {country.population}</div>
                                 <div>Region: {country.region}</div>
                                 <div>Capital: {country.capital}</div>
